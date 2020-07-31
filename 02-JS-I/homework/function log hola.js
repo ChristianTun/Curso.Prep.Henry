7e95045bf85aed3237a0531401bcfba5c5e6bca1
ChristianTun <ChristianTun>
2020-07-31 00:59:20 -0300

    finish homework

---
+++ 02-JS-I/homework/function log hola.js
@@ -0,0 +1,49 @@
+function loghola() {
+	console.log("hola");
+}
+
+loghola();
+
+
+function logHola(nombre) {
+    console.log('Hola, ' + nombre);
+}
+
+logHola('Martin');
+
+
+
+
+function suma(x, y) {
+var g = x + y ;
+return(g)
+
+	}
+
+	function tienenMismaLongitud(str1, str2) {
+  // Devuelve "true" si las dos strings tienen la misma longitud
+  // De lo contrario, devuelve "false"
+  // Tu código:
+  var casa = str1;
+  var casa2 = str2;
+
+  if (casa.legth == casa2.legth) {
+  	return true
+  	}
+  	return false
+}
+
+function tienenMismaLongitud(str1) {
+  // Devuelve "true" si las dos strings tienen la misma longitud
+  // De lo contrario, devuelve "false"
+  // Tu código:
+  var nombre ='str1'
+  console log('nombre.length');
+}
+
+
+
+
+
+
+
